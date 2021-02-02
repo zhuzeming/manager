87e25a70c594f7b0bf7165399ebc24b25ed0d688
小明的GitHub <36179769+zhuzeming@users.noreply.github.com>
2021-02-02 17:59:00 +0800

    i++

---
+++ manager-web/src/main/java/com/demo/Test.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+/**
+ * @author ZeMing Zhu
+ * @date Create in 2021/2/2 13:41
+ * @description 请添加描述
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 0;
+//        i = i++;
+//        i = ++i;
+        i++;
+//        ++i;
+        System.out.println(i);
+    }
+}
