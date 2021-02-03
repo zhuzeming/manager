ea0d19399a5f354ddd59c3f9327531f2ee63160a
小明的GitHub <36179769+zhuzeming@users.noreply.github.com>
2021-02-03 12:09:23 +0800

    i++ ++i

--- manager-web/src/main/java/com/demo/Test.java
+++ manager-web/src/main/java/com/demo/Test.java
@@ -8,10 +8,12 @@ package com.demo;
 public class Test {
     public static void main(String[] args) {
         int i = 0;
+        System.out.println(i++);
+//        System.out.println(++i);
+//        i++;
+//        ++i;
 //        i = i++;
 //        i = ++i;
-        i++;
-//        ++i;
-        System.out.println(i);
+//        System.out.println(i);
     }
 }
